0b3b92feef66316ae37d5494513c38d03841e734
Krishna <krishna@evolveasia.co>
2018-12-23 12:56:01 +0545

    customize google map

--- GoogleMapSamples/ViewController.swift
+++ GoogleMapSamples/ViewController.swift
@@ -10,22 +10,25 @@ import UIKit
 import GoogleMaps
 
 class ViewController: UIViewController {
-
+//    @IBOutlet weak var uiView: UIView!
+    @IBOutlet weak var mapVIew: GMSMapView!
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 14.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
+//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+//        uiView.addSubview(mapView)
+        self.mapVIew.camera = camera
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
         marker.title = "Sydney"
         marker.snippet = "Australia"
-        marker.map = mapView
+        marker.map = self.mapVIew
     }
     
     
